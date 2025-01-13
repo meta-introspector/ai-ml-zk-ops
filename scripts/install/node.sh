fa4856824ca504718b2e2c4dae38a59a4cf30b9d
mike dupont <mike.dupont@introspector.local>
2025-01-13 11:32:13 -0500

    v1

---
+++ scripts/install/node.sh
@@ -0,0 +1,5 @@
+
+curl -fsSL https://get.pnpm.io/install.sh | env PNPM_VERSION=10.0.0 sh -
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+nvm install 23
+nvm use 23
