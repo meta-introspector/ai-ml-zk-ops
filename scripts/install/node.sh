c670e4c9335a0cb82d52a5d5e446e0fc8cef5991
Mike DuPont <jmikedupont2@gmail.com>
2025-03-18 10:28:06 -0400

    Update node.sh

--- scripts/install/node.sh
+++ scripts/install/node.sh
@@ -2,10 +2,8 @@
 
 export BASHRC="$HOME/.bashrc"
 export PNPM_VERSION=10.0.0
-export APP_DIR=/opt/agent
-
-cd "${APP_DIR}" || echo "you need to set APP_DIR to where you app is like /opt/agent"
-
+#export APP_DIR=/opt/agent
+#cd "${APP_DIR}" || echo "you need to set APP_DIR to where you app is like /opt/agent"
 curl -fsSL https://get.pnpm.io/install.sh | sh -
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
