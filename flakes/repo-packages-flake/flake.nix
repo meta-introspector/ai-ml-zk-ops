b269b38b4c75b49840d06c5f0608cd0b5c9e07ae
mike <h@solfunmeme.com>
2025-09-24 23:51:27 +0000

    feat: Temporarily modify flake.nix for repoData debugging

--- flakes/repo-packages-flake/flake.nix
+++ flakes/repo-packages-flake/flake.nix
@@ -11,24 +11,40 @@
     flake-utils-wrapper.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs-pinned.legacyPackages.${system};
-        repoData = repo-data-flake.lib.${system}.repoData;
+        traceJson = prefix: value: builtins.trace (prefix + ": " + builtins.toJSON value) {};
+        repoData = traceJson "repoData" repo-data-flake.lib.${system}.repoData;
       in
       {
-        packages = builtins.listToAttrs (map (item: {
-          name = "${item.owner}-${item.repo}";
-          value = pkgs.stdenv.mkDerivation {
-            pname = "${item.repo}";
-            version = "unstable"; # Or derive from git tags/commits
-            src = pkgs.fetchFromGitHub {
-              owner = item.owner;
-              repo = item.repo;
-              ref = "feature/CRQ-016-nixify"; # Using the specified ref
-              # sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # Placeholder, needs to be updated
-            };
-            dontBuild = true;
-            dontInstall = true;
-          };
-        }) repoData);
+        # Temporarily commented out to debug repoData
+        # packages = builtins.listToAttrs (map (item: {
+        #   name = "${item.owner}-${item.repo}";
+        #   value = pkgs.stdenv.mkDerivation {
+        #     pname = "${item.repo}";
+        #     version = "unstable"; # Or derive from git tags/commits
+        #     src = pkgs.fetchFromGitHub {
+        #       owner = item.owner;
+        #       repo = item.repo;
+        #       ref = "feature/CRQ-016-nixify"; # Using the specified ref
+        #       # sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # Placeholder, needs to be updated
+        #     };
+        #     dontBuild = true;
+        #     dontInstall = true;
+        #   };
+        # }) repoData);
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+          ];
+          
+        };
+
+        packages.dummy = pkgs.stdenv.mkDerivation {
+          pname = "dummy-package";
+          version = "1.0";
+          src = pkgs.lib.cleanSource ./.;
+          installPhase = "mkdir -p $out/bin; echo 'Hello from dummy' > $out/bin/dummy";
+        };
+        repoData = repoData; # Expose repoData for inspection
       }
     );
 }
